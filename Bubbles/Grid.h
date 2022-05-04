0546aebd05e133d7b811a227e1748c4c7db632a1
Karl Trueman <karltrueman1997@gmail.com>
2022-05-05 00:34:27 +0100

    update

--- Bubbles/Grid.h
+++ Bubbles/Grid.h
@@ -6,17 +6,17 @@
 class grid
 {
 public:
-	int gridHeight;
-	int gridWidth;
+	int gridHeight = 0;
+	int gridWidth = 0;
 	int columns = 14;
 	int rows = 12;
 	int radius = 15;
 	std::vector<std::vector<Bubble*>> g_tiles;
 	bool BubblesInterest(int row, int column);
 	void DeletedBubbles(int row, int column, sf::Color TargetColour);
-	int destroyedBalls;
-	int tileWidth;
-	int tileHeight;
+	int destroyedBalls = 0;
+	int tileWidth = 0;
+	int tileHeight = 0;
 };
 
 
